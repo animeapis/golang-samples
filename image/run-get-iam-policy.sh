e609b54e0e8c06c992cf4dacb4e9e3488960b75d
Christian Roggia <christian.roggia@gmail.com>
2022-08-22 23:24:27 +0200

    feat: add more samples to Image API

---
+++ image/run-get-iam-policy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+go build \
+  -o golang-samples \
+  -ldflags="
+    -X 'main.ClientID=$(cat .oauth-client-id)'
+    -X 'main.ClientSecret=$(cat .oauth-client-secret)'
+    -X 'main.Album=users/6880568923540230144/albums/6956454702873989120'
+    -X 'main.TokenURL=https://accounts.animeshon.dev/o/oauth2/token'
+    -X 'main.Endpoint=image.animeapis.dev:443'
+  " \
+  ./image/set-iam-policy
+
+./golang-samples
+
+exit 0
