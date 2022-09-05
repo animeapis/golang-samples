aad603b90df21c4921f856b9ec60f83738b218f1
Christian Roggia <christian.roggia@gmail.com>
2022-09-05 17:25:08 +0200

    fix: fetch cookie from ignored file

--- iam/run-create-service-account-with-cookie.sh
+++ iam/run-create-service-account-with-cookie.sh
@@ -10,7 +10,7 @@ go build \
     -X 'main.ServiceAccountName=users/6880568923540230144/serviceAccounts/test-3'
     -X 'main.ServiceAccountDescription=Test'
     -X 'main.ServiceAccountDisplayName=Test'
-    -X 'main.Cookie=ory_kratos_session=MTY2MTEyMDc5OHw0Qkx1NC1BU09MOXd1OVdrcTNuR052blhRVEZwN19rSzJpNzZQcVllcVV6anUyTUZmTk9aUjd4NmZpOEFxckhabWZ4WTNzQXgwTXNJUTBLYlFZc190ZndienBBTmp0SUxiMWwzUlBRRW9KRTBTN2E3SnZiQ3Ficlc2eWVjeDg4akNNVlFUSHAtUFE9PXzCXTQ6LS3EBB9YXc1IZ_jtbsERBK7_wcSB8LDlNXHjKQ=='
+    -X 'main.Cookie=$(cat .test-cookie)'
     -X 'main.Endpoint=iam.animeapis.dev:443'
   " \
   ./iam/create-service-account-with-cookie
